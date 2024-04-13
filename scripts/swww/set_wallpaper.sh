703a79cffb5f15a03a0aa0bc49b11cc055bca1cf
ndrewA <andrewbach1999@gmail.com>
2024-04-14 02:07:59 +0300

    major fixes for hyprland

--- scripts/swww/set_wallpaper.sh
+++ scripts/swww/set_wallpaper.sh
@@ -16,15 +16,24 @@ else
     fi
 fi
 
-# Function to set wallpaper and update the system theme.
+# Function to set wallpaper, update the system theme, and apply a random transition position.
 set_wallpaper() {
     echo "$1" > "$LAST_WALLPAPER_FILE" # Save the current wallpaper path
-    swww img "$1" --transition-fps 60 --transition-type center
+
+    # Generate random transition position coordinates
+    RANDOM_X=$(awk -v min=0 -v max=1 'BEGIN{srand(); print min+rand()*(max-min)}')
+    RANDOM_Y=$(awk -v min=0 -v max=1 'BEGIN{srand(); print min+rand()*(max-min)}')
+
+    # Apply the wallpaper with a random transition position
+    swww img "$1" --transition-pos $RANDOM_X,$RANDOM_Y --transition-fps 90 --transition-type grow
+
     wal -i "$1"  # Generate and apply theme with Pywal
     /home/andrew/.config/dunst/apply_pywall_theme.sh  # Apply Pywal theme to Dunst
     killall waybar
-    waybar
+    waybar &
 }
 
 # Apply the wallpaper.
 set_wallpaper "$SPECIFIC_WALLPAPER"
+
+exit 0
